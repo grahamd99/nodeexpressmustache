0e1bbd13766e89b25fe47bcdb762f1696934f7c3
grahamd99 <graham.dodsworth@nhs.net>
2023-08-31 13:40:26 +0100

    Add all the js files and templates

---
+++ simpleStorage.js
@@ -0,0 +1,9 @@
+var globalValue = "";
+function getValue() {
+  return globalValue;
+}
+function setValue(value) {
+  globalValue = value;
+}
+module.exports.getValue = getValue;
+module.exports.setValue = setValue;
